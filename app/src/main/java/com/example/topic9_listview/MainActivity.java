0ff14f775e73288214d14bc06d60dc3516bbe14a
jjaca <jjaca@cifpfbmoll.eu>
2022-01-26 17:45:48 +0100

    ListView estático

---
+++ app/src/main/java/com/example/topic9_listview/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.topic9_listview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.ListActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+public class MainActivity extends ListActivity {
+    private TextView selection;
+    private static final String[] items={"lorem", "ipsum", "dolor",
+            "sit", "amet",
+            "consectetuer", "adipiscing", "elit", "morbi", "vel",
+            "ligula", "vitae", "arcu", "aliquet", "mollis",
+            "etiam", "vel", "erat", "placerat", "ante",
+            "porttitor", "sodales", "pellentesque", "augue", "purus"};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setListAdapter(new ArrayAdapter<String>(this, R.layout.row, R.id.textView_name , items));
+        selection=(TextView)findViewById(R.id.selection);
+    }
+
+    public void onListItemClick(ListView parent, View v, int position, long id) {
+        selection.setText(items[position]);
+    }
+
+
+}
